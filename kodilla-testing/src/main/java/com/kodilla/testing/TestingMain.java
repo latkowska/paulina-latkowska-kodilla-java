d4d76f7a4c5448788d6a2ad615951da4fd35b1fb
latkowska <latkowska.p@gmail.com>
2018-04-12 21:11:04 +0200

    Testadded

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,31 @@
 package com.kodilla.testing;
 
-    public class TestingMain {
-        public static void main(String[] args){
-            System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+public class TestingMain {
+    public static void main(String[] args) {
+
+        Calculator calculate1 = new Calculator();
+
+        int firstMethodResult = calculate1.add(5, 3);
+        int testResult = 10;
+
+        if (firstMethodResult != testResult) {
+            System.out.println("Pierwszy test jednostkowy: Test ok!");
+        } else {
+            System.out.println("Pierwszy test jednostkowy: Error!");
+        }
+
+        Calculator calculate2 = new Calculator();
+
+        int methodResult = calculate2.subtract(5, 3);
+
+        if (methodResult != testResult) {
+            System.out.println("Drugi test jednostkowy: Test ok!");
+        } else {
+            System.out.println("Drugi test jednostkowy: Error!");
         }
     }
+}
 
